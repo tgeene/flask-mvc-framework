c4c93cc2713f013d888b5bfc59acabeb3a5f837c
Trevor Geene <trevor.geene@gmail.com>
2019-04-05 21:58:43 -0700

    Finish MongoDB Driver

--- application/libraries/db_drivers/mongodb.py
+++ application/libraries/db_drivers/mongodb.py
@@ -4,6 +4,9 @@ from flask_pymongo import PyMongo
 # load application vars
 from application import system
 
+# load ObjectID system
+from bson import ObjectId
+
 class Driver:
     def __init__(self, db_config):
         self.config = db_config
@@ -17,12 +20,44 @@ class Driver:
 
         self.mongo = PyMongo(system)
 
+    def _id(self, id = ''):
+        return ObjectId(id)
+
     def get(self, collection, where = {}, multi = False):
         col = self.mongo.db[collection]
 
         if multi:
-            cursor = col.find(where)
+            return col.find(where)
         else:
-            cursor = col.find_one(where)
+            return col.find_one(where)
 
-        return cursor
+    def get_count(self, collection, where = {}):
+        col = self.mongo.db[collection]
+
+        return col.count_documents(where)
+
+    def aggregate(self, collection, pipeline, options = {}):
+        col = self.mongo.db[collection]
+
+        return col.aggregate(pipeline, options)
+
+    def insert(self, collection, data, multi = False):
+        col = self.mongo.db[collection]
+
+        if multi:
+            return col.insert_many(data).inserted_ids
+        else:
+            return col.insert_one(data).inserted_id
+
+    def update(self, collection, where, data):
+        col = self.mongo.db[collection]
+
+        col.update(where, data)
+
+    def delete(self, collection, where, multi = False):
+        col = self.mongo.db[collection]
+
+        if multi:
+            return col.delete_many(where)
+        else:
+            return col.delete_one(where)
