fe34f1efbcbf2a06067546fdd63bbfcda6bb79b6
Trevor Geene <trevor.geene@gmail.com>
2019-05-14 19:09:54 -0700

    Add `werkzeug` for loading Flask

--- app.py
+++ app.py
@@ -3,4 +3,5 @@ from application import system
 
 # run application
 if __name__ == '__main__':
-    system.run()
+    from werkzeug.serving import run_simple
+    run_simple('127.0.0.1', 5000, system, use_reloader=True, use_debugger=True, use_evalex=True)
